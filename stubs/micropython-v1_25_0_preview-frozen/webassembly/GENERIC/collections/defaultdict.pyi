7da4656b20b97ac3efb19f9c8fe222d7107af4c7
bobvan <bobvan@users.noreply.github.com>
2025-02-27 01:36:41 +0000

    update stubs

--- stubs/micropython-v1_25_0_preview-frozen/webassembly/GENERIC/collections/defaultdict.pyi
+++ stubs/micropython-v1_25_0_preview-frozen/webassembly/GENERIC/collections/defaultdict.pyi
@@ -1,4 +1,17 @@
+"""
+Collection and container types.
+
+MicroPython module: https://docs.micropython.org/en/v1.25.0-preview/library/collections.html
+
+CPython module: :mod:`python:collections` https://docs.python.org/3/library/collections.html .
+
+This module implements advanced collection and container types to
+hold/accumulate various objects.
+"""
+
+from __future__ import annotations
 from _typeshed import Incomplete
+from stdlib.collections import OrderedDict as stdlib_OrderedDict, deque as stdlib_deque, namedtuple as stdlib_namedtuple
 
 class defaultdict:
     d: Incomplete
