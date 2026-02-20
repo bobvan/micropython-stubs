9bc2f25fba9dc7168c1e8f7f1104200228020fa6
bobvan <bobvan@users.noreply.github.com>
2026-02-20 01:42:06 +0000

    update stubs

--- stubs/micropython-v1_27_0-frozen/esp32/ARDUINO_NANO_ESP32/machine.pyi
+++ stubs/micropython-v1_27_0-frozen/esp32/ARDUINO_NANO_ESP32/machine.pyi
@@ -14,12 +14,11 @@ damage.
 from __future__ import annotations
 from _typeshed import Incomplete
 from micropython import const as const
-from typing_extensions import Awaitable, TypeAlias, TypeVar
 from _mpy_shed import _IRQ, AnyReadableBuf, AnyWritableBuf
+from typing_extensions import Awaitable, TypeAlias, TypeVar
 
 _path: Incomplete
 _PCNT_RANGE: int
-ATTN_0DB: int = ...
 IDLE: Incomplete
 """IRQ wake values."""
 SLEEP: Incomplete
@@ -42,6 +41,7 @@ PIN_WAKE: Incomplete
 """Wake-up reasons."""
 RTC_WAKE: Incomplete
 """Wake-up reasons."""
+ATTN_0DB: int = ...
 
 class _CounterBase:
     _PCNT: Incomplete
